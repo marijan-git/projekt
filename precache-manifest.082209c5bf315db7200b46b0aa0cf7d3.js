0b4a148cfdfc9dbeffbc351836e6d8be142138e5
marijan-git <msimovi@gmail.com>
2020-10-08 13:54:04 +0200

    Updates

---
+++ precache-manifest.082209c5bf315db7200b46b0aa0cf7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b989f346e79259492402c3fdbba6f5a7",
+    "url": "/projekt/index.html"
+  },
+  {
+    "revision": "8793b71ce81599b42c0c",
+    "url": "/projekt/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "c0b9ea2cf0cb15bc8a7a",
+    "url": "/projekt/static/js/2.29497ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projekt/static/js/2.29497ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8793b71ce81599b42c0c",
+    "url": "/projekt/static/js/main.e9e6dd3b.chunk.js"
+  },
+  {
+    "revision": "20cd5fa76e0a9557b77b",
+    "url": "/projekt/static/js/runtime-main.40d20913.js"
+  }
+]);
